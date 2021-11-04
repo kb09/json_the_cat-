54d447129b39bb206824aefcb28fdd8089a90421
kartik <kabakshi@hotmail.ca>
2021-11-04 13:26:20 -0400

    URL which will return cat breed results

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,9 @@
 // require the request library,
 const request = require('request');
+
+//URL which will return cat breed results.
+const fetchBreedDescription = function(breedName, callback) {
+
+  const url = 'https://api.thecatapi.com/v1/breeds/search';
+
+}
