c2697269ff31be61d1f729f5b63be59108cacd8a
kartik <kabakshi@hotmail.ca>
2021-11-04 13:40:02 -0400

    breed not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,14 +6,27 @@ const breedData = function(breedName, callback) {
 
   const url = 'https://api.thecatapi.com/v1/breeds/search';
 
-  //Access the first entry in the data array and print out the description
-
-  const descrp = data[0].description
-  }
-
   // convert the JSON string into an actual object
 
   const data = JSON.parse(body);
   console.log(data);
   console.log(typeof data);
-}
+
+  //Access the first entry in the data array and print out the description
+
+  const descrp = data[0].description
+
+  if(data[0]) {
+      let desc = data[0]['description'];
+      return callback(null,desc);
+    } else {
+       let noDesc = "Breed not found"; // if breed name not found 
+       return callback(noDesc,null);
+    }
+
+  } // end of breedData function
+
+  
+
+
+  
