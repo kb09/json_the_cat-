37e8abf919ff5df2fb15f52435a268408bb5af9f
kartik <kabakshi@hotmail.ca>
2021-11-04 13:54:45 -0400

    breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,36 +6,34 @@ const breedData = function(breedName, callback) {
 
   const url = 'https://api.thecatapi.com/v1/breeds/search?q=sib';
 
-  request(url, function(error, respnose, body){
+  request(url, function(error, respnose, body) {
     // if error
 
-    if(error) {
-      return callback(error,null); 
+    if (error) {
+      return callback(error,null);
     
     }
     
-  // convert the JSON string into an actual object
+    // convert the JSON string into an actual object
 
-  const data = JSON.parse(body);
+    const data = JSON.parse(body);
   
-  console.log(data);
-  console.log(typeof data);
+    console.log(data);
+    console.log(typeof data);
 
-  //Access the first entry in the data array and print out the description
+    //Access the first entry in the data array and print out the description
 
-  // let descrpt = data[0].description
+    // let descrpt = data[0].description
   
-  if(data[0]) {
+    if (data[0]) {
       let descpt = data[0]['description'];
       return callback(null,descpt);
     } else {
-       let noDescpt = "Breed not found"; // if breed name not found 
-       return callback(noDescpt,null);
+      let noDescpt = "Breed not found"; // if breed name not found
+      return callback(noDescpt,null);
     }
-  }); // end of request function 
+  }); // end of request function
 }; // end of breedData function
 
   
-
-
-  
+module.exports = { breedData };
